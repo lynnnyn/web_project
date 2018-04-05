007c84176b2278bfadf71b41516dc8b67a7460c9
nyn88@163.com <NYN84302448@>
2018-04-04 21:12:20 -0500

    add  sort by mix attr

--- FlaskApp 3/app.py
+++ FlaskApp 3/app.py
@@ -2,7 +2,8 @@ from flask import Flask, request, redirect, url_for, render_template,flash,sessi
 from flask_sqlalchemy import SQLAlchemy
 import sys
 from flask.ext.wtf import Form
-import dbconnect
+# import dbconnect
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
 app = Flask(__name__)
@@ -53,45 +54,101 @@ class Food(db.Model):
 # 主页
 @app.route('/', methods = ['GET','POST'])
 def home():
-
-    if request.method == "POST":
-        search = request.form['search']
-        print(search)
-        # user_id = request.args.get('')
-        return render_template('home.html', nearby = {},recommendation = {},search = search)
-
-    elif request.method == 'GET':
-        search = 'coffee'
-        sql_search = 'SELECT * FROM Food WHERE name = "%s" ' % search
-        location = 'urbana'
-        sql_nearby = 'SELECT * FROM Food WHERE area = "%s" ' % location
-        prefer = 'lynn'
-        # sql_prefer = 'SELECT * FROM User left join Preference on User.id = Preference.user_id WHERE User.name = "%s" ' % prefer
-        sql_prefer = 'SELECT * FROM User left join Preference on User.user_id = Preference.user_id WHERE User.user_name = "%s" ' % prefer
-
-
+    user_id = 1
+    user_name = hzong2
+    user_zipcode = 61801
 
     def process_dict(inputlist, attr):
         outputlist = []
         for i in inputlist:
-            dict_list = dict(zip(attr,i))
+            dict_list = dict(zip(attr, i))
             outputlist.append(dict_list)
         return outputlist
 
-
     attr_food   = ('id', 'name', 'area', 'price', 'maker_id', 'description', 'available_amount', 'score', 'category_id')
-    attr_prefer = ('id', 'name','password_hash','email','phone_number','address','gender','selling_score','purchasing_score','zipcode','user_id','category_id')
+    attr_search = ('name', 'area','price','description','available_amount', 'score','AVG_PRICE','AVG_SCORE','seller_score')
 
-    search_list = db.session.execute(sql_search).fetchall()
+    if request.method == "POST":
+        search          = request.form['search']
+        price_all       = request.form['price']    #return true/false
+        price_low       = request.form['price_low']
+        price_high      = request.form['price_high']
+        foodscore_all   = request.form['foodscore']
+        foodscore_low   = request.form['foodscore_low']
+        foodscore_high  = request.form['foodscore_high']
+
+        result  = {'Food.price': {'select': 'descent', 'low': '3', 'high': '10'},
+                  'Food.score': {'select': 'ascent', 'low': '2', 'high': ''},
+                  'User.selling_score': {'select': 'null', 'low': '', 'high': ''},
+                  'Food.available_amount': {'select': 'ascent', 'low': '1', 'high': '5'}}
+
+        sort    = ''
+        where   = ''
+        for attr in result:
+            # print('attr',attr)
+            # print('result[attr]',result[attr])
+            # print(result[attr]['select'])
+            if result[attr]['select'] == 'descent':
+                sort = sort + attr + ' ' + 'DESC,'
+            elif result[attr]['select'] == 'ascent':
+                sort = sort + ' ' + attr + ','
+
+            if result[attr]['low'] != '':
+                # print('result[attr][low]',result[attr]['low'])
+                where = where + ' ' + 'AND' + ' ' + attr + ">" + result[attr]['low']
+
+            if result[attr]['high'] != '':
+                # print('result[attr][high]',result[attr]['high'])
+                where = where + ' ' + 'AND' + ' ' + attr + "<" + result[attr]['high']
+
+        sort = sort[:-1]
+        print(sort)
+        print(where)
+
+
+
+
+
+
+
+
+
+
+        print(search)
+        # user_id = request.args.get('')
+        # sql_search  = 'SELECT * FROM Food WHERE name = "%s" ' % search
+        # sql_search  = '(SELECT Food.name,Food.area,Food.price,Food.description,Food.available_amount,Food.score,AVG_PRICE,AVG_SCORE,User.selling_score FROM Food LEFT JOIN(SELECT Food.name, Food.area ,AVG(Food.price) AS AVG_PRICE,AVG(Food.score) AS AVG_SCORE FROM Food GROUP BY Food.name, Food.area) NEW_FOOD ON Food.name = NEW_FOOD.name AND Food.area = NEW_FOOD.area LEFT JOIN User ON Food.maker_id = User.id LEFT JOIN Category ON Food.category_id = Category.id WHERE Food.name LIKE "%%%s%%" OR Category.name LIKE "%%%s%%" OR User.name LIKE "%%%s%%" OR Food.area = "%s" ORDER BY Food.score DESC,Food.price,User.selling_score DESC,Food.available_amount DESC)' % (search,search,search,search)
+        sql_search  = '(SELECT Food.name,Food.area,Food.price,Food.description,Food.available_amount,Food.score,AVG_PRICE,AVG_SCORE,User.selling_score FROM Food LEFT JOIN(SELECT Food.name, Food.area ,AVG(Food.price) AS AVG_PRICE,AVG(Food.score) AS AVG_SCORE FROM Food GROUP BY Food.name, Food.area) NEW_FOOD ON Food.name = NEW_FOOD.name AND Food.area = NEW_FOOD.area LEFT JOIN User ON Food.maker_id = User.id LEFT JOIN Category ON Food.category_id = Category.id WHERE Food.name LIKE "%%%s%%" OR Category.name LIKE "%%%s%%" OR User.name LIKE "%%%s%%" OR Food.area = "%s" "%s" ORDER BY "%s")' % (search,search,search,search,where,sort)
+        search_list = db.session.execute(sql_search).fetchall()
+        search_l    = process_dict(search_list, attr_food)
+        return render_template('home.html', nearby = {},recommendation = {},search = search_l)
+
+    elif request.method == 'GET':
+        # search = 'coffee'
+        # sql_search = 'SELECT * FROM Food WHERE name = "%s" ' % search
+        location   = '61820'
+        # location = user_zipcode
+        # sql_nearby = 'SELECT * FROM Food WHERE area = "%s" ' % location
+
+        prefer     = 'nyn88'
+        # prefer = user_name
+        # sql_prefer = 'SELECT * FROM User left join Preference on User.id = Preference.user_id WHERE User.name = "%s" ' % prefer
+        # sql_prefer = 'SELECT * FROM Food left join User on  left join Preference on User.user_id = Preference.user_id WHERE User.user_name = "%s" ' % prefer
+        sql_prefer = '(SELECT * FROM Food WHERE Food.category_id IN (SELECT DISTINCT Food.category_id FROM Suborder LEFT JOIN Orders ON Suborder.order_id = Orders.id LEFT JOIN User ON Orders.buyer_id = User.id LEFT JOIN Food ON Food.id = Suborder.food_id WHERE User.name = '%s')) UNION (SELECT * FROM Food WHERE Food.category_id IN (SELECT DISTINCT Food.category_id FROM Food LEFT JOIN Preference ON Food.category_id = Preference.category_id LEFT JOIN User ON Preference.user_id = User.id WHERE User.name = '%s' AND Food.area = User.zipcode))' % (prefer,prefer)
+
+    # attr_food   = ('id', 'name', 'area', 'price', 'maker_id', 'description', 'available_amount', 'score', 'category_id')
+    # attr_prefer = ('id', 'name','password_hash','email','phone_number','address','gender','selling_score','purchasing_score','zipcode','user_id','category_id')
+
+    # search_list = db.session.execute(sql_search).fetchall()
     nearby_list = db.session.execute(sql_nearby).fetchall()
     prefer_list = db.session.execute(sql_prefer).fetchall()
-    search_l    = process_dict(search_list, attr_food)
+    # search_l    = process_dict(search_list, attr_food)
     nearby_l    = process_dict(nearby_list, attr_food)
-    prefer_l    = process_dict(prefer_list, attr_prefer)
+    prefer_l    = process_dict(prefer_list, attr_food)
     # print(prefer_l)
 
 
-    return render_template('search.html', nearby = nearby_l, recommendation = prefer_l,search = search_l)
+    return render_template('home.html', nearby = nearby_l, recommendation = prefer_l,search = {})
 
 
 
@@ -160,6 +217,41 @@ def home():
 #             result_str = 'No preferred food!'
 #         return render_template('prefer.html', result=result_str, food_list=food_list)
 #
+
+# profile
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+
+    user_name = hzong2
+
+    def process_dict(inputlist, attr):
+        outputlist = []
+        for i in inputlist:
+            dict_list = dict(zip(attr, i))
+            outputlist.append(dict_list)
+        return outputlist
+
+    attr_food = ('id', 'name', 'area', 'price', 'maker_id', 'description', 'available_amount', 'score', 'category_id')
+
+    if request.method == "POST":
+        description = request.form['search']
+        print(search)
+        # user_id = request.args.get('')
+        # sql_search  = 'SELECT * FROM Food WHERE name = "%s" ' % search
+        sql_search = 'SELECT Food.name,Food.area,Food.price,Food.description,Food.available_amount,Food.score,AVG_PRICE,AVG_SCORE, User.selling_score FROM Food LEFT JOIN(SELECT Food.name,AVG(Food.price) AS AVG_PRICE,AVG(Food.score) AS AVG_SCORE FROM Food GROUP BY Food.name) NEW_FOOD ON Food.name = NEW_FOOD.name LEFT JOIN User ON Food.maker_id = User.id WHERE Food.name = ' % s
+        ' ORDER BY Food.price,Food.score DESC,User.selling_score DESC' % search
+        search_list = db.session.execute(sql_search).fetchall()
+        search_l = process_dict(search_list, attr_food)
+        return render_template('home.html', nearby={}, recommendation={}, search=search_l)
+
+    elif request.method == 'GET':
+
+        sql_maker = 'SELECT * FROM `Food` WHERE Food.maker_id = (SELECT User.id FROM User WHERE User.name = '%s')' % user_name
+
+    maker_list = db.session.execute(sql_maker).fetchall()
+    maker_l  = process_dict(maker_list, attr_food)
+
+    return render_template('profile.html', maker = maker_l)
 
 
 if __name__ == "__main__":

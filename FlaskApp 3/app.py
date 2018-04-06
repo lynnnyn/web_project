405ead6eac04f0882f9e4f7d5a748318774833b4
lynnnyn <yinanni2@illinois.edu>
2018-04-06 17:46:12 -0500

    add default rank to search

--- FlaskApp 3/app.py
+++ FlaskApp 3/app.py
@@ -69,7 +69,7 @@ def home():
     attr_search = ('name', 'area','price','description','available_amount', 'score','AVG_PRICE','AVG_SCORE','seller_score')
 
     if request.method == "POST":
-        search          = request.form['search']
+        # search          = request.form['search']
         # price_all       = request.form['price']    #return true/false
         # price_low       = request.form['price_low']
         # price_high      = request.form['price_high']
@@ -77,17 +77,21 @@ def home():
         # foodscore_low   = request.form['foodscore_low']
         # foodscore_high  = request.form['foodscore_high']
 
-        result  = {'Food.price': {'select': 'descent', 'low': '3', 'high': '10'},
+        result = {'search': 'sdf',
+                  'Food.price': {'select': 'descent', 'low': '3', 'high': '10'},
                   'Food.score': {'select': 'ascent', 'low': '2', 'high': ''},
                   'User.selling_score': {'select': 'null', 'low': '', 'high': ''},
                   'Food.available_amount': {'select': 'ascent', 'low': '1', 'high': '5'}}
-
-        sort    = ''
-        where   = ''
+        result = request.json
+        sort = ''
+        where = ''
         for attr in result:
+            if attr == 'search':
+                continue
             # print('attr',attr)
             # print('result[attr]',result[attr])
             # print(result[attr]['select'])
+
             if result[attr]['select'] == 'descent':
                 sort = sort + attr + ' ' + 'DESC,'
             elif result[attr]['select'] == 'ascent':
@@ -100,22 +104,20 @@ def home():
             if result[attr]['high'] != '':
                 # print('result[attr][high]',result[attr]['high'])
                 where = where + ' ' + 'AND' + ' ' + attr + "<" + result[attr]['high']
+        search = result['search']
 
         sort = sort[:-1]
-        print(sort)
-        print(where)
 
-
-
-        print(search)
         # user_id = request.args.get('')
         # sql_search  = 'SELECT * FROM Food WHERE name = "%s" ' % search
         # sql_search  = '(SELECT Food.name,Food.area,Food.price,Food.description,Food.available_amount,Food.score,AVG_PRICE,AVG_SCORE,User.selling_score FROM Food LEFT JOIN(SELECT Food.name, Food.area ,AVG(Food.price) AS AVG_PRICE,AVG(Food.score) AS AVG_SCORE FROM Food GROUP BY Food.name, Food.area) NEW_FOOD ON Food.name = NEW_FOOD.name AND Food.area = NEW_FOOD.area LEFT JOIN User ON Food.maker_id = User.id LEFT JOIN Category ON Food.category_id = Category.id WHERE Food.name LIKE "%%%s%%" OR Category.name LIKE "%%%s%%" OR User.name LIKE "%%%s%%" OR Food.area = "%s" ORDER BY Food.score DESC,Food.price,User.selling_score DESC,Food.available_amount DESC)' % (search,search,search,search)
-        sql_search  = '(SELECT Food.name,Food.area,Food.price,Food.description,Food.available_amount,Food.score,AVG_PRICE,AVG_SCORE,User.selling_score FROM Food LEFT JOIN(SELECT Food.name, Food.area ,AVG(Food.price) AS AVG_PRICE,AVG(Food.score) AS AVG_SCORE FROM Food GROUP BY Food.name, Food.area) NEW_FOOD ON Food.name = NEW_FOOD.name AND Food.area = NEW_FOOD.area LEFT JOIN User ON Food.maker_id = User.id LEFT JOIN Category ON Food.category_id = Category.id WHERE Food.name LIKE "%%%s%%" OR Category.name LIKE "%%%s%%" OR User.name LIKE "%%%s%%" OR Food.area = "%s" "%s" ORDER BY "%s")' % (search,search,search,search,where,sort)
+        # sql_search  = '(SELECT Food.name,Food.area,Food.price,Food.description,Food.available_amount,Food.score,AVG_PRICE,AVG_SCORE,User.selling_score FROM Food LEFT JOIN(SELECT Food.name, Food.area ,AVG(Food.price) AS AVG_PRICE,AVG(Food.score) AS AVG_SCORE FROM Food GROUP BY Food.name, Food.area) NEW_FOOD ON Food.name = NEW_FOOD.name AND Food.area = NEW_FOOD.area LEFT JOIN User ON Food.maker_id = User.id LEFT JOIN Category ON Food.category_id = Category.id WHERE Food.name LIKE "%%%s%%" OR Category.name LIKE "%%%s%%" OR User.name LIKE "%%%s%%" OR Food.area = "%s" "%s" ORDER BY "%s")' % (search,search,search,search,where,sort)
+        sql_search  = '(SELECT DISTINCT Food.name,Food.area,Food.price,Food.description,Food.available_amount,Food.score,AVG_PRICE,AVG_SCORE,User.selling_score FROM Food LEFT JOIN(SELECT Food.name, Food.area ,AVG(Food.price) AS AVG_PRICE,AVG(Food.score) AS AVG_SCORE FROM Food GROUP BY Food.name, Food.area) NEW_FOOD ON Food.name = NEW_FOOD.name AND Food.area = NEW_FOOD.area LEFT JOIN User ON Food.maker_id = User.id LEFT JOIN Food_Category ON Food.id = Food_Category.food_id LEFT JOIN Category ON Food_Category.category_id = Category.id WHERE Food.name LIKE "%%%s%%" OR Category.name LIKE "%%%s%%" OR User.name LIKE "%%%s%%" OR Food.area = "%s" %s ORDER BY %s)' % (search,search,search,search,where,sort)
+        print(sql_search)
         search_list = db.session.execute(sql_search).fetchall()
-        search_l    = process_dict(search_list, attr_food)
+        search_l    = process_dict(search_list, attr_search)
+        print(search_l)
         return render_template('home.html', nearby = {},recommendation = {},search = search_l)
-
 
     elif request.method == 'GET':
 
@@ -123,9 +125,9 @@ def home():
 
         sql_nearby = 'SELECT * FROM Food LEFT JOIN User ON Food.area = User.zipcode WHERE User.name = "%s"' % user
 
-        sql_prefer = '(SELECT * FROM Food WHERE Food.category_id IN (SELECT DISTINCT Food.category_id FROM Suborder LEFT JOIN Orders ON Suborder.order_id = Orders.id LEFT JOIN User ON Orders.buyer_id = User.id LEFT JOIN Food ON Food.id = Suborder.food_id WHERE User.name = "%s")) UNION (SELECT * FROM Food WHERE Food.category_id IN (SELECT DISTINCT Food.category_id FROM Food LEFT JOIN Preference ON Food.category_id = Preference.category_id LEFT JOIN User ON Preference.user_id = User.id WHERE User.name = "%s" AND Food.area = User.zipcode))' % (user,user)
+        # sql_prefer = '(SELECT * FROM Food WHERE Food.category_id IN (SELECT DISTINCT Food.category_id FROM Suborder LEFT JOIN Orders ON Suborder.order_id = Orders.id LEFT JOIN User ON Orders.buyer_id = User.id LEFT JOIN Food ON Food.id = Suborder.food_id WHERE User.name = "%s")) UNION (SELECT * FROM Food WHERE Food.category_id IN (SELECT DISTINCT Food.category_id FROM Food LEFT JOIN Preference ON Food.category_id = Preference.category_id LEFT JOIN User ON Preference.user_id = User.id WHERE User.name = "%s" AND Food.area = User.zipcode))' % (user,user)
+        sql_prefer = '(SELECT DISTINCT Food.* FROM Food, Food_Category WHERE Food_Category.food_id = Food.id AND Food_Category.category_id IN (SELECT DISTINCT Food_Category.category_id FROM Food_Category, Orders LEFT JOIN User ON Orders.buyer_id = User.id LEFT JOIN Food ON Food.id = Orders.food_id WHERE User.name = "nyn88" AND Food_Category.food_id = Food.id)) UNION(SELECT DISTINCT Food.* FROM Food,Food_Category WHERE Food.id = Food_Category.food_id AND Food_Category.category_id IN( SELECT DISTINCT Food_Category.category_id FROM Food_Category,User,Preference,Food WHERE User.id = Preference.user_id AND Food_Category.category_id = Preference.category_id AND User.id = Preference.user_id AND Food_Category.food_id = Food.id AND User.name = "nyn88" AND User.zipcode = Food.area))'
 
-    nearby_list = db.session.execute(sql_nearby).fetchall()
     prefer_list = db.session.execute(sql_prefer).fetchall()
     nearby_l    = process_dict(nearby_list, attr_food)
     prefer_l    = process_dict(prefer_list, attr_food)
